d8272e18c2560d562af6eb6af2c47685cc85b0b3
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-08-16 18:21:46 +0200

    Gravity changes to the character, had some difficulties cause of the way i implemented it

--- player_character.gd
+++ player_character.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
-const SPEED = 120.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 160.0
+var jump_velocity = -475.0
+var gravity = 2300
+var jumptween
+signal changegravity
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if is_on_floor():
+		PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY, gravity)
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		gravity = 2300
+		jumptween = get_tree().create_tween()
+		jumptween.tween_property(self, "gravity", 750, 0.2)
+		jumptween.play()
+		velocity.y += jump_velocity
+		
+	if Input.is_action_just_released("jump"):
+		jumptween.stop()
 		
 	var direction := Input.get_axis("left", "right")
 	if direction:
@@ -16,4 +29,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
+	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY, gravity)
 	move_and_slide()
