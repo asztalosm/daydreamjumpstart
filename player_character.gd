6cee4250cb2832cdb928ff85a7448771a23a6382
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-08-23 00:49:43 +0200

    Final push to release, added 2 levels, clouds, radio tower

--- player_character.gd
+++ player_character.gd
@@ -6,14 +6,17 @@ var gravity = 2300
 var jumptween
 var jumping = false
 var hasjumped = false # just an extra failsafe so that when the player falls but has not yet jumped won't crash the game, shouldnt happen but whoever tests it would find the crash if i didnt fix it
+var haswrench = false
 
+func _ready():
+	print(get_tree().current_scene)
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if is_on_floor():
 		jumping = false
 		PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY, gravity)
-		
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jumping = true
 		gravity = 2300
@@ -50,3 +53,21 @@ func _physics_process(delta: float) -> void:
 		
 	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY, gravity)
 	move_and_slide()
+
+
+func _on_wrench_body_entered(body: Node2D) -> void:
+	haswrench = true
+	get_parent().get_node("Wrench").queue_free()
+
+
+func _on_radio_tower_body_entered(body: Node2D) -> void:
+	if !haswrench:
+		$Camera2D/Tutorial/TextureRect4.visible = true
+		$Camera2D/Tutorial/TextureRect4/RichTextLabel.text = "Get the wrench before progressing to the next level"
+		await get_tree().create_timer(2).timeout
+		$Camera2D/Tutorial/TextureRect4.visible = false
+	else:
+		if get_tree().current_scene.name == "Level1":
+			get_tree().change_scene_to_file('res://level2.tscn')
+		elif get_tree().current_scene.name == "Level2":
+			get_tree().change_scene_to_file("res://end.tscn")
